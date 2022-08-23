fc493269aed8e77afe3454af34ee103c395936d4
aesxcision <111344164+aesxcision@users.noreply.github.com>
2022-08-23 14:42:51 +0800

    Create App.java

---
+++ Aug 23/Without Constructor/App.java
@@ -0,0 +1,49 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+        knight.sayMyStrength();
+        knight.sayMyAgility();
+        knight.sayMyintelligence();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+        mage.sayMyStrength();
+        mage.sayMyAgility();
+        mage.sayMyintelligence();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+        thief.sayMyStrength();
+        thief.sayMyAgility();
+        thief.sayMyintelligence();
+
+        Character healer = new Character();
+
+        healer.strength = 2;
+        healer.agility = 4;
+        healer.intelligence = 5;
+        healer.name = "Sora";
+        healer.sayMyName();
+        healer.sayMyStrength();
+        healer.sayMyAgility();
+        healer.sayMyintelligence();
+
+    }
+}
